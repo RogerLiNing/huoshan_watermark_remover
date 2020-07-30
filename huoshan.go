b3e3c4f45768e8b14d385c467c2205f1dd0615fa
Roger Lee <rogerlee0421@gmail.com>
2020-07-31 00:17:40 +0800

    Update huoshan.go

--- huoshan.go
+++ huoshan.go
@@ -3,7 +3,11 @@ package huoshan
 func WatermarkRemover(url string)(string, error)  {
 
 	videoId, err := getVideoId(url)
-
+	
+	if len(videoId) == 0 {
+		return "", nil
+	}
+	
 	videoLink, err := getVideoLink(videoId)
 
 	downloadLink, err := getVideoDownloadLink(videoLink)
